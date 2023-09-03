ebb9d958477cb4f1ea4a6eb731933f3c52a0e8b3
maritocuate <mario300@gmail.com>
2023-09-03 02:47:46 -0300

    chore: add table pagination

--- src/components/StocksTable/index.tsx
+++ src/components/StocksTable/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import ReactPaginate from 'react-paginate'
 import { css } from '@emotion/css'
 
 import { Apiresults } from "../../types"
@@ -11,40 +13,59 @@ interface StocksTableProps {
 
 const StocksTable = ({ data, handleDetails }: StocksTableProps) => {
     const stocks = data?.data || []
+
+    const [currentPage, setCurrentPage] = useState<number>(0)
+    const itemsPerPage = 15
+    const indexOfLastItem = (currentPage + 1) * itemsPerPage
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage
+    const currentItems = stocks.slice(indexOfFirstItem, indexOfLastItem)
+
+    const handlePageChange = (selectedPage: { selected: number }) => {
+        setCurrentPage(selectedPage.selected)
+    }
   
     return (
-        <table width='100%'>
-            <thead>
-                <tr>
-                    <th>Simbolo</th>
-                    <th>Nombre</th>
-                    <th>Moneda</th>
-                    <th>Tipo</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    stocks.map((stock, index) => {
-                        const bg = index%2===0 ? '#444' : '#222'
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Simbolo</th>
+                        <th>Nombre</th>
+                        <th>Moneda</th>
+                        <th>Tipo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        currentItems.map((stock, index) => {
+                            const bg = index%2===0 ? '#444' : '#222'
 
-                        return (
-                            <tr
-                                key={index}
-                                className={css`background-color: ${bg}`}
-                            >
-                                <td
-                                    className='linked'
-                                    onClick={() => handleDetails(stock.symbol)}
-                                >{stock.symbol}</td>
-                                <td>{stock.name}</td>
-                                <td>{stock.currency}</td>
-                                <td>{stock.type}</td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-        </table>
+                            return (
+                                <tr
+                                    key={index}
+                                    className={css`background-color: ${bg}`}
+                                >
+                                    <td
+                                        className='linked'
+                                        onClick={() => handleDetails(stock.symbol)}
+                                    >{stock.symbol}</td>
+                                    <td>{stock.name}</td>
+                                    <td>{stock.currency}</td>
+                                    <td>{stock.type}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+
+            <ReactPaginate
+                pageCount={Math.ceil(stocks.length / itemsPerPage)}
+                onPageChange={handlePageChange}
+                containerClassName='pagination'
+                activeClassName='active'
+            />
+        </>
     )
 }
   
